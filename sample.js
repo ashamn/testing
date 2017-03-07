943fb31edf0bdeb65dbc2442958c37ad09e9d9a3
ashamn <whthupnd@gmail.com>
2017-03-07 13:52:28 +0800

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('sample')[0].style.visibility = 'hidden';
+document.getElementById("opt2").parentNode.style.display='none';
+document.getElementById("opt1").parentNode.className = 'col-md-6 col-xs-12 col-sm-6';
+document.getElementById("opt3").parentNode.className = 'col-md-6 col-xs-12 col-sm-6';
