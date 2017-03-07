d86bb03ceb266d259943935547143d0f65816611
ashamn <whthupnd@gmail.com>
2017-03-07 14:03:46 +0800

    Update and rename sample1.js to sample2.js

--- sample1.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("opt2").parentNode.style.display='none';
-document.getElementById("opt1").parentNode.className = 'col-md-6 col-xs-12 col-sm-6';
-document.getElementById("opt3").parentNode.className = 'col-md-6 col-xs-12 col-sm-6';
---
+++ sample2.js
@@ -0,0 +1,3 @@
+document.getElementById("opt1").parentNode.style.display='none';
+document.getElementById("opt2").parentNode.className = 'col-md-6 col-xs-12 col-sm-6';
+document.getElementById("opt3").parentNode.className = 'col-md-6 col-xs-12 col-sm-6';
